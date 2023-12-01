ddcc59bb430e61f7db8648a16e64bca299bad51e
kietnguyen1998 <nguyenvuanhkiet9191@gmail.com>
2023-12-01 08:49:24 +0700

    mine

---
+++ store/notification-context.js
@@ -0,0 +1,16 @@
+import { createContext } from "react";
+
+const NotificationContext = createContext({
+  notification: null,
+  showNotification: () => {},
+  hideNotification: () => {},
+});
+
+export const NotificationContextProvider = (props) => {
+  return (
+    <NotificationContext.Provider>
+      {props.children}
+    </NotificationContext.Provider>
+  );
+};
+export default NotificationContext;
